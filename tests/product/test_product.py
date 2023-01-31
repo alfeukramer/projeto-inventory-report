a846a9fbac396bf24b73a5b4e00eb475db30e455
alfeukramer <alfeukramer@gmail.com>
2023-01-31 16:37:22 -0300

    test: Cria teste verifica se está criando corretamente product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass
+    inventory_product = Product(
+        "1",
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1",
+        )
+
+    mock_return = (
+            "O produto Nicotine Polacrilex"
+            " fabricado em 2021-02-18"
+            " por Target Corporation com validade"
+            " até 2023-09-17"
+            " precisa ser armazenado instrucao 1."
+    )
+
+    assert str(inventory_product) == mock_return
