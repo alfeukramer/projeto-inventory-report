2d04f122ea6a5bfb96b692f9bf3d639059247f64
alfeukramer <alfeukramer@gmail.com>
2023-02-06 23:09:07 -0300

    teste avaliador

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -3,6 +3,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.json_importer import JsonImporter
 from inventory_report.importer.xml_importer import XmlImporter
+# forçando push novamente
 
 
 class Inventory():
