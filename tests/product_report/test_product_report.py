cb7bf197ba46ce44fb513e9606ebff2209472123
alfeukramer <alfeukramer@gmail.com>
2023-02-06 23:25:47 -0300

    test: Implementa teste retorno padrão

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mock = Product(
+        id=1,
+        nome_do_produto="Cafe",
+        nome_da_empresa="Cafes Nature",
+        data_de_fabricacao="2020-07-04",
+        data_de_validade="2023-02-09",
+        numero_de_serie="FR48",
+        instrucoes_de_armazenamento="instrucao",
+    )
+
+    mock_return = (
+        "O produto Cafe fabricado em 2020-07-04 por Cafes Nature"
+        " com validade até 2023-02-09 precisa ser armazenado instrucao."
+    )
+
+    assert str(mock) == mock_return
